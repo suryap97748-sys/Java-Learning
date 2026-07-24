87c8c1358864b36436f19432c34bb6c27bcc4851
suryap97748-sys <suryap97748@gmail.com>
2026-07-24 20:25:55 +0530

    Delete Hello.java
    This program adds two numbers using scanner

--- Hello.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-   public class Hello {
-    public static void main(String[] args) {
-   Scanner s=new Scanner(System.in);
-     int a=s.nextInt();
-      int b=s.nextInt();
-        System.out.println(a+b);
-    }
-}
