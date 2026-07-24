d8f486b6ee03a146f0f96a2a7b4668682d4867fd
suryap97748-sys <suryap97748@gmail.com>
2026-07-24 20:14:21 +0530

    Create Hello.java
    This program adds two numbers using scanner

---
+++ Hello.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+   public class Hello {
+    public static void main(String[] args) {
+   Scanner s=new Scanner(System.in);
+     int a=s.nextInt();
+      int b=s.next.Int();
+        System.out.println(a+b);
+    }
+}
